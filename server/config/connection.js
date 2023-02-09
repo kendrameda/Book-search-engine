d20180cb121e63729931ecc37434336f048c0cd2
Kendrameda <gibsoken@gmail.com>
2023-02-09 17:27:49 -0600

    Updated code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/kendras-book-engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/kendras-book-engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
